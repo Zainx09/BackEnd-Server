b53723fc47ee1fc3067f2534b5250be1148156ec
zain ul abdin <zainulabdin.x09@gmail.com>
2022-04-05 21:16:27 +0500

    deploying on heroku

--- src/index.js
+++ src/index.js
@@ -97,6 +97,5 @@ app.get('/', (req, res) => {
     res.send("Server Is Working......")
 })
 
-app.listen(3000, () => {
-    console.log("Server is Running on port 3000")
-})
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
